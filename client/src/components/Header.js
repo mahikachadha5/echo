670017f63ac42fd61871e42dc24de05a955bd764
mahikachadha5 <chadhama5@gmail.com>
2026-02-23 15:14:38 -0500

    update header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,14 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <header className="header">
-    <div className="logo">
-      <div className="logo-mark">E</div>
       <span className="logo-name">Echo</span>
-    </div>
-    <div className="header-right">
-      <div className="ai-dot" />
-      <span>Powered by Claude</span>
-    </div>
   </header>
 );
 
