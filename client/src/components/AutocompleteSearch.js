67a419d1241ce2c62d886d9483dd137b754ab453
mahikachadha5 <chadhama5@gmail.com>
2026-02-23 12:31:24 -0500

    update autocomplete logic

--- client/src/components/AutocompleteSearch.js
+++ client/src/components/AutocompleteSearch.js
@@ -1,41 +1,44 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import searchIcon from '../images/searchIcon.png';
 import '../styles.css';
-import axios from 'axios';
 
 const AutocompleteSearch = ({ onPlaceSelected }) => {
     const inputRef = useRef(null);
 
     useEffect(() => {
-        if (!window.google) return;
+        let interval;
 
-        const input = document.getElementById('search-input');
+        const init = () => {
+            if (!window.google?.maps?.places) return false;
 
-        const autocomplete = new window.google.maps.places.Autocomplete(inputRef.current, {
-            fields: ["place_id", "name", "formatted_address"],
-            types: ["establishment"]
-        });
+            const autocomplete = new window.google.maps.places.Autocomplete(inputRef.current, {
+                fields: ["place_id", "name", "formatted_address"],
+                types: ["establishment"]
+            });
 
-        autocomplete.addListener('place_changed', () => {
-            const place = autocomplete.getPlace();
-            if (place.place_id) {
-                onPlaceSelected(place);
-            }
-        });
-}, [onPlaceSelected]);
+            autocomplete.addListener('place_changed', () => {
+                const place = autocomplete.getPlace();
+                if (place.place_id) onPlaceSelected(place);
+            });
 
-  return (
-    <div className='center-container'>
-      <div className='search-container'>
-        <img src={searchIcon} className='search-icon' alt='Search Icon'/>
-        <input
-            ref={inputRef}
-            type="text"
-            placeholder="Search for a place"
-        />
-      </div>
-    </div>
-  );
+            return true;
+        };
+
+        if (!init()) {
+            interval = setInterval(() => { if (init()) clearInterval(interval); }, 100);
+        }
+
+        return () => clearInterval(interval);
+    }, [onPlaceSelected]);
+
+    return (
+        <div className='center-container'>
+            <div className='search-container'>
+                <img src={searchIcon} className='search-icon' alt='Search Icon' />
+                <input ref={inputRef} type="text" placeholder="Search for a place" />
+            </div>
+        </div>
+    );
 };
 
 export default AutocompleteSearch;
